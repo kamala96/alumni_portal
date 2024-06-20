35465710f86eb9667732483c0b1104720628e439
Jovin Kamala <jovin@jovin.jovin.com>
2024-06-20 11:55:31 +0300

    Integratinng Template - Jovin

--- site_app/views.py
+++ site_app/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # sliders = Slider.objects.order_by('-created_at')[:7]
+    # sliders = Slider.objects.filter(is_active=True).order_by('-created_at')
+    # accounting_officer = AccountingOfficer.load()
+
+    context = {
+    }
+    return render(request, 'index.html', context)
