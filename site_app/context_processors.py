666b068b8faca7af391b75bff7848c4affa9deaa
kassim <kassimhussein066@gmail.com>
2024-06-25 09:27:15 +0300

    nav pages

--- site_app/context_processors.py
+++ site_app/context_processors.py
@@ -1,5 +1,12 @@
 from django.conf import settings
+from site_app.models import *
 
 
 def site_name(request):
     return {'site_name': settings.SITE_NAME}
+
+
+# def menu_context(request):
+#     # Retrieve all menu items
+#     top_menus = Navigationmenu.objects.filter(parent_menu__isnull=True, is_visible=True)
+#     return {'menus': top_menus}
