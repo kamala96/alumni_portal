049ad418fb2df1a45813b7b459525f919e68f0fc
kassim <kassimhussein066@gmail.com>
2024-06-25 09:47:10 +0300

    comited

--- site_app/context_processors.py
+++ site_app/context_processors.py
@@ -8,9 +8,3 @@ def site_name(request):
 def navigation_menu(request):
     main_menu = Navigationmenu.objects.filter(menu_type='main', is_active=True).order_by('order_id')
     return {'main_menu': main_menu}
-
-
-# def menu_context(request):
-#     # Retrieve all menu items
-#     top_menus = Navigationmenu.objects.filter(parent_menu__isnull=True, is_visible=True)
-#     return {'menus': top_menus}
